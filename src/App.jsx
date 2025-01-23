88d34c22d00e81b5eda2a22a7cc42f05fe98b2c8
nirsingh <nirireads@gmail.com>
2025-01-23 23:32:14 +0545

    added/500-vocab

--- src/App.jsx
+++ src/App.jsx
@@ -34,70 +34,77 @@ const App = () => {
       </div>
     ),
     backHTML: (
-      <div className="back-content  p-2 flex flex-col items-center justify-center h-full text-center">
-        <div className="pedagogy text-2xl font-bold italic text-blue-900 mb-4">
+      <div className="back-content  p-2 flex flex-col items-center justify-center h-full text-center ">
+        <div className="pedagogy text-[1rem] lg:text-2x1 font-bold italic text-blue-900 mb-4">
           {word.meaning}
         </div>
-        <div className="meaning text-lg text-gray-700 mb-3 text-center">
+        <div className="meaning text-[1rem] lg:text-lg text-gray-700 mb-3 text-center">
           {word.formation}
         </div>
-        <div className="sentence text-md text-pink-600">"{word.sentence}"</div>
+        <div className="sentence text-[1rem] lg:text-md text-pink-600">
+          "{word.sentence}"
+        </div>
       </div>
     ),
   }));
 
   return (
-    <div className="app-container flex item-center justify-center ">
-      <FlashcardArray
-        cards={cards}
-        controls={true}
-        showCount={true}
-        FlashcardArrayStyle={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          gap: "1rem",
-        }}
-        frontCardStyle={{
-          background: "linear-gradient(135deg, #4facfe 0%, #00f2fe 100%)",
-          color: "#fff",
-          padding: "1.5rem",
-          borderRadius: "15px",
-          boxShadow: "0 8px 20px rgba(0, 0, 0, 0.2)",
-          fontFamily: "'Poppins', sans-serif",
-          fontSize: "1.5rem",
-          textAlign: "center",
-        }}
-        backCardStyle={{
-          background: "#f5f5f5",
-          color: "#333",
-          padding: "1.5rem",
-          borderRadius: "15px",
-          boxShadow: "0 8px 20px rgba(0, 0, 0, 0.2)",
-          fontFamily: "'Poppins', sans-serif",
-          fontSize: "1.2rem",
-          lineHeight: "1.8",
-        }}
-        frontContentStyle={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100%",
-        }}
-        backContentStyle={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "0.8rem",
-        }}
-        onCardChange={(id, index) =>
-          console.log(`Card Changed: ID=${id}, Index=${index}`)
-        }
-      />
-      {offset + chunkSize < vocabulary.length && (
-        <button className="load-more-btn" onClick={handleNextChunk}>
-          Load More Words
-        </button>
-      )}
+    <div className="app-container flex flex-col item-center justify-center border border-black h-screen w-screen">
+      <div className="flex item-center justify-center">
+        <FlashcardArray
+          cards={cards}
+          controls={true}
+          showCount={true}
+          FlashcardArrayStyle={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            gap: "1rem",
+          }}
+          frontCardStyle={{
+            background: "linear-gradient(135deg, #4facfe 0%, #00f2fe 100%)",
+            color: "#fff",
+            padding: "1.5rem",
+            borderRadius: "15px",
+            boxShadow: "0 8px 20px rgba(0, 0, 0, 0.2)",
+            fontFamily: "'Poppins', sans-serif",
+            fontSize: "1.5rem",
+            textAlign: "center",
+          }}
+          backCardStyle={{
+            background: "#f5f5f5",
+            color: "#333",
+            padding: "1.5rem",
+            borderRadius: "15px",
+            boxShadow: "0 8px 20px rgba(0, 0, 0, 0.2)",
+            fontFamily: "'Poppins', sans-serif",
+            fontSize: "1.2rem",
+            lineHeight: "1.8",
+          }}
+          frontContentStyle={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+          }}
+          backContentStyle={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.8rem",
+          }}
+          onCardChange={(id, index) =>
+            console.log(`Card Changed: ID=${id}, Index=${index}`)
+          }
+        />
+      </div>
+
+      <div className="flex justify-center">
+        {offset + chunkSize < vocabulary.length && (
+          <button className="load-more-btn w-fit" onClick={handleNextChunk}>
+            Load More Words
+          </button>
+        )}
+      </div>
     </div>
   );
 };
